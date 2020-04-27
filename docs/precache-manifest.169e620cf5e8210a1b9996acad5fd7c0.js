1cc1cc280766392b0acd32d958411b771880f243
Edison Alba <edison.alba@imaginamos.com.co>
2020-04-27 09:25:31 -0500

    deploy new page

---
+++ docs/precache-manifest.169e620cf5e8210a1b9996acad5fd7c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0735a0e87faef53175332b7e9c0f1341",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b2b6cd10a937648cd0f",
+    "url": "/static/css/2.22d6b5ec.chunk.css"
+  },
+  {
+    "revision": "2ed9a274fa277e729905",
+    "url": "/static/css/main.1009db2e.chunk.css"
+  },
+  {
+    "revision": "0b2b6cd10a937648cd0f",
+    "url": "/static/js/2.2c50d548.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/static/js/2.2c50d548.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2ed9a274fa277e729905",
+    "url": "/static/js/main.e7635401.chunk.js"
+  },
+  {
+    "revision": "5f07a09bebc73af33ebd",
+    "url": "/static/js/runtime-main.374eeb9d.js"
+  }
+]);
