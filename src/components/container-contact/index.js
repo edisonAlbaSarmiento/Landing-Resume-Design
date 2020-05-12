1155f5295db47dd6580899d2c50efe1e8fdb685d
Edison Alba <edison.alba@imaginamos.com.co>
2020-05-12 11:57:30 -0500

    add exapmple formik

--- src/components/container-contact/index.js
+++ src/components/container-contact/index.js
@@ -27,7 +27,8 @@ const comapny = 'imaginamos'
 const position ='npse'
 const desdripcion  =' sjdshdkshdkjsahdslahdls'
 
-
+  // const response = await sendFeedback('sendgrid', {message_html: `${name}<br/> <br/> ${comapny}<br/> ${position}<br/> ${desdripcion}<br/>`, from_name: `${email}`, reply_to: 'edison.alba@imaginamos.com.co'})
+  // console.log('RESP=OS', response.status)
 class Contact extends React.Component {
   constructor(props) {
     super(props);
@@ -125,3 +126,68 @@ Contact.propTypes = {
 }
 
 export default Contact;
+/* eslint-disable react/jsx-filename-extension */
+// import React from 'react'; ejmplo de form formik
+// import { Formik } from 'formik';
+
+// const Basic = () => (
+//   <div>
+//     <h1>Anywhere in your app!</h1>
+//     <Formik
+//       initialValues={{ email: '', password: '' }}
+//       validate={values => {
+//         const errors = {};
+//         if (!values.email) {
+//           errors.email = 'Required';
+//         } else if (
+//           !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+//         ) {
+//           errors.email = 'Invalid email address';
+//         }
+//         return errors;
+//       }}
+//       onSubmit={(values, { setSubmitting }) => {
+//         setTimeout(() => {
+//           alert(JSON.stringify(values, null, 2));
+//           setSubmitting(false);
+//         }, 1000);
+//       }}
+//     >
+//       {({
+//         values,
+//         errors,
+//         touched,
+//         handleChange,
+//         handleBlur,
+//         handleSubmit,
+//         isSubmitting,
+//         /* and other goodies */
+//       }) => (
+//         <form onSubmit={handleSubmit}>
+//           <input
+//             type="email"
+//             name="email"
+//             onChange={handleChange}
+//             onBlur={handleBlur}
+//             value={values.email}
+//           />
+//           {errors.email && touched.email && errors.email}
+//           <input
+//             type="password"
+//             name="password"
+//             onChange={handleChange}
+//             onBlur={handleBlur}
+//             value={values.password}
+//           />
+//           {errors.password && touched.password && errors.password}
+//           <button type="submit" disabled={isSubmitting}>
+//             Submit
+//           </button>
+//         </form>
+//       )}
+//     </Formik>
+//   </div>
+// );
+
+// export default Basic;
+
