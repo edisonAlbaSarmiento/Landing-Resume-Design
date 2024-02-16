c425e52efbf24d9b976c5725622e2074f02dd59d
Edison Alba <edalbasa@poligran.edu.co>
2024-02-16 17:40:08 -0500

    validate delploy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,6 @@ export const metadata: Metadata = {
 	title: "Edison Hoja de Vida",
 	description: "Mi hoja de vida",
 	manifest: "/public/manifest.json",
-	viewport: "width=device-width,initial-scale=1",
 };
 
 export default function RootLayout({
